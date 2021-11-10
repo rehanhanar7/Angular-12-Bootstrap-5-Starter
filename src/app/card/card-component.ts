5e9dad816b67b0804454dc8f22c6ddb2ba79b822
rehanhanar7 <rehanhanar7@gmail.com>
2021-11-10 16:33:17 +0530

    3 files modified

--- src/app/card/card-component.ts
+++ src/app/card/card-component.ts
@@ -5,4 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: 'card-component.html',
   styleUrls: ['card-component.scss'],
 })
-export class CardComponent {}
+export class CardComponent {
+  cards = [
+    {
+      imagesrc:
+        'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+      title: 'Lens',
+      desc: 'Best Lens in the world',
+    },
+    {
+      imagesrc:
+        'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+      title: 'Lens',
+      desc: 'Best Lens in the world',
+    },
+    {
+      imagesrc:
+        'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+      title: 'Lens',
+      desc: 'Best Lens in the world',
+    },
+  ];
+}
