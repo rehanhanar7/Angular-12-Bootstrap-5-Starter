fad526abb3ddf35a8734ca63d6720b23812c0040
rehanhanar7 <rehanhanar7@gmail.com>
2021-11-10 17:07:38 +0530

    2 files modified

--- src/app/home/home-component.ts
+++ src/app/home/home-component.ts
@@ -5,24 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: 'home-component.html',
   styleUrls: ['home-component.scss'],
 })
-export class HomeComponent {
-  activeIndex: any;
-  images = [
-    {
-      src: 'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
-      name: 'image1',
-    },
-    {
-      src: 'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
-      name: 'image2',
-    },
-    {
-      src: 'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
-      name: 'image3',
-    },
-  ];
-
-  setIndex(index) {
-    this.activeIndex = index;
-  }
-}
+export class HomeComponent {}
