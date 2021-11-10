0aca52c60ecfd794cabeafc40175f0d5bfe2db68
rehanhanar7 <rehanhanar7@gmail.com>
2021-11-10 17:16:53 +0530

    1 file modified

--- src/app/card/card-component.ts
+++ src/app/card/card-component.ts
@@ -11,19 +11,19 @@ export class CardComponent {
       imagesrc:
         'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
       title: 'Planning a few trips?',
-      desc: 'Best Lens in the world',
+      desc: 'With our rail passes offer you benefit from flexibility an affordable price and special perks!',
     },
     {
       imagesrc:
         'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
       title: 'Tuesdays are made for saving',
-      desc: 'Best Lens in the world',
+      desc: 'With our rail passes offer you benefit from flexibility an affordable price and special perks!',
     },
     {
       imagesrc:
         'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
       title: 'Lowest fares',
-      desc: 'Best Lens in the world',
+      desc: 'Travelling by train is affordable. Take advantage of our best rates and book your comeback.',
     },
   ];
 }
